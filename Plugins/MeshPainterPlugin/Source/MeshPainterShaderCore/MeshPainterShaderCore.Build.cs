7a9358b100e9a8ea9c51a354845daed5802ca411
Oleg <fishelev@hustleshift.net>
2024-11-13 18:10:21 +0500

    update

--- Plugins/MeshPainterPlugin/Source/MeshPainterShaderCore/MeshPainterShaderCore.Build.cs
+++ Plugins/MeshPainterPlugin/Source/MeshPainterShaderCore/MeshPainterShaderCore.Build.cs
@@ -8,48 +8,9 @@ public class MeshPainterShaderCore : ModuleRules
     {
         PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        //		PublicIncludePaths.AddRange(
-        //			new string[] {
-        //				// ... add public include paths required here ...
-        //			}
-        //			);
-
-        //		PrivateIncludePaths.AddRange(
-        //			new string[] {
-        //				// ... add other private include paths required here ...
-        //			}
-        //			);
         PrivateIncludePaths.AddRange(new string[] { Path.Combine(GetModuleDirectory("Renderer"), "Private"), });
-
-        //		PublicDependencyModuleNames.AddRange(
-        //			new string[]
-        //			{
-        //				"Core",
-        //				// ... add other public dependencies that you statically link with here ...
-        //			}
-        //			);
         PublicDependencyModuleNames.AddRange(new string[] { "Core" });
-
-        //		PrivateDependencyModuleNames.AddRange(
-        //			new string[]
-        //			{
-        //				"CoreUObject",
-        //				"Engine",
-        //				"Slate",
-        //				"SlateCore",
-        //				// ... add private dependencies that you statically link with here ...	
-        //			}
-        //			);
         PrivateDependencyModuleNames.AddRange(new string[] { "CoreUObject", "Engine", "Projects", "RenderCore", "Renderer", "RHI" });
-
-        //		DynamicallyLoadedModuleNames.AddRange(
-        //			new string[]
-        //			{
-        //				// ... add any modules that your module loads dynamically here ...
-        //			}
-        //			);
-        //	}
-        //}
         DynamicallyLoadedModuleNames.AddRange(new string[] { });
     }
 }
