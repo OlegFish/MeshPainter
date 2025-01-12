f57a9d01dd64ddf420b116065c30b1e38f1b2dd3
Oleg <ofishelev@gmail.com>
2025-01-12 19:14:38 +0500

    update 12.01.25

--- Plugins/MeshPainterPlugin/Source/MeshPainterShaderCore/Public/Shaders/SingleColorShader.h
+++ Plugins/MeshPainterPlugin/Source/MeshPainterShaderCore/Public/Shaders/SingleColorShader.h
@@ -1,17 +1,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "ShaderParameterStruct.h"
 
 BEGIN_SHADER_PARAMETER_STRUCT(FSingleColorParameters, )
 SHADER_PARAMETER(FVector3f, Color)
+SHADER_PARAMETER(float, Time)
 END_SHADER_PARAMETER_STRUCT()
-
-void FFF()
-{
-	FSingleColorParameters X;
-	X.Color = FVector3f(FColor::Red);
-}
-
 
 class FSingleColorShaderVS : public FGlobalShader
 {
@@ -26,5 +21,38 @@ public:
 	{
 		FGlobalShader::ModifyCompilationEnvironment(InParameters, OutEnvironment);
 		//OutEnvironment.SetDefine(TEXT("TILE_SIZE"), FComputeShaderUtils::kGolden2DGroupSize);
+	}
+};
+
+class FSingleColorShaderPS : public FGlobalShader
+{
+	DECLARE_GLOBAL_SHADER(FSingleColorShaderPS);
+	SHADER_USE_PARAMETER_STRUCT(FSingleColorShaderPS, FGlobalShader);
+
+	using FParameters = FSingleColorParameters;
+	//typedef FSingleColorParameters FParameters;
+
+public:
+	static void ModifyCompilationEnvironment(const FGlobalShaderPermutationParameters& InParameters, FShaderCompilerEnvironment& OutEnvironment)
+	{
+		FGlobalShader::ModifyCompilationEnvironment(InParameters, OutEnvironment);
+		//OutEnvironment.SetDefine(TEXT("TILE_SIZE"), FComputeShaderUtils::kGolden2DGroupSize);
+	}
+};
+
+class FSingleColorShaderCS : public FGlobalShader
+{
+	DECLARE_GLOBAL_SHADER(FSingleColorShaderCS);
+	SHADER_USE_PARAMETER_STRUCT(FSingleColorShaderCS, FGlobalShader);
+
+	using FParameters = FSingleColorParameters;
+	//typedef FSingleColorParameters FParameters;
+
+public:
+	static void ModifyCompilationEnvironment(const FGlobalShaderPermutationParameters& InParameters, FShaderCompilerEnvironment& OutEnvironment)
+	{
+		FGlobalShader::ModifyCompilationEnvironment(InParameters, OutEnvironment);
+		OutEnvironment.SetDefine(TEXT("NUM_THREADS_X"), 1);
+		OutEnvironment.SetDefine(TEXT("NUM_THREADS_Y"), 1);
 	}
 };
